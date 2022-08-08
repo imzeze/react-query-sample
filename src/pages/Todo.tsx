e34b9189c6a5a86e9c307e03995c66e15bae9a07
imzeze <jihyekim019@gmail.com>
2022-08-08 16:21:31 +0900

    feat: mutation 사용 - 자동저장

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,40 +1,85 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
 import { QUERY_KEY } from "../constants/queryKey";
 import { TodoModel, PaginationModel } from "../models";
 
-/**
- * useQuery를 이용한 pagination
- * keepPreviousData 사용
- *
- * @returns todoList
- */
-const Todo = () => {
-  const [page, setPage] = useState(0);
+interface TodoItemProps {
+  todo: TodoModel;
+}
 
+const Todo = () => {
+  const [page, setPage] = useState(1);
+
+  /**
+   * useQuery를 이용한 pagination
+   * keepPreviousData 사용
+   *
+   * @returns todoList
+   */
   const { status, data } = useQuery({
     queryKey: [QUERY_KEY.TODO_LIST_QUERY, page],
     queryFn: async (): Promise<{ todos: TodoModel[] } & PaginationModel> => {
-      return await fetch(
-        `https://dummyjson.com/todos?limit=10&skip=${page * 10}`
-      ).then((res) => res.json());
+      return await fetch(`https://dummyjson.com/todos?limit=${page * 10}`).then(
+        (res) => res.json()
+      );
     },
     keepPreviousData: true,
   });
+
+  /**
+   * auto save
+   */
 
   if (status === "loading") return <>Loading</>;
   return (
     <div>
       {data?.todos.map((todo) => (
-        <div key={`${todo.userId}_${todo.id}`}>{todo.todo}</div>
+        <React.Fragment key={`${todo.userId}_${todo.id}`}>
+          <TodoItem todo={todo} />
+        </React.Fragment>
       ))}
       <div>
-        <ul>
-          <li onClick={() => setPage(0)}>1</li>
-          <li onClick={() => setPage(1)}>2</li>
-          <li onClick={() => setPage(2)}>3</li>
-        </ul>
+        <button onClick={() => setPage(page + 1)}>더보기</button>
       </div>
+    </div>
+  );
+};
+
+const TodoItem = ({ todo }: TodoItemProps) => {
+  const [info, setInfo] = useState(todo);
+
+  const onUpdateSubmit = useMutation(async (data: TodoModel) => {
+    return await fetch(`https://dummyjson.com/todos/${data.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: data.todo,
+      }),
+    }).then((res) => res.json());
+  });
+
+  const updateContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInfo((prev) => {
+      return { ...prev, todo: value };
+    });
+  };
+
+  // setState 동기처럼 처리
+  useEffect(() => {
+    if (todo.todo !== info.todo) {
+      onUpdateSubmit.mutate(info);
+    }
+  }, [info.todo]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={info.todo}
+        size={100}
+        onChange={(e) => updateContent(e)}
+      />
     </div>
   );
 };
